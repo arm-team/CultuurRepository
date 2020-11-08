f3124cd02a2df8bb20b68283836bc6f944333689
achmadilyasa <achmadilyasa99@gmail.com>
2020-11-08 22:54:25 +0700

    Merubah tampilan HOME serta ada perubahan kecil di Sign Up

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,7 +8,7 @@ import {Router} from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  today = Date.now();
   constructor(
     private authService: AuthenticationService,
     private router: Router,
